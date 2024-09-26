7e256c6f812991c1da3dd31d25c3200ff43a2eed
Curlytop2point0 <maximcalvinperson3@gmail.com>
2024-09-26 13:02:04 -0400

    01 Assignment: Course Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// getdates.js
+
+// Update the current year in the footer
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+// Update the last modified date in the footer
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
